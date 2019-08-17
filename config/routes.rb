be50d8fe1502eb6af5d37014d8ff6a5eaddeb89a
HiromuAndo <tqgqxxjuzaa8hbcwjv7n@docomo.ne.jp>
2019-08-18 08:09:41 +0900

    [Add]ユーザー詳細・ユーザー編集ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   	registrations: 'users/registrations'
   }
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
